e4fb9a9d9374ad546b9fd56cb0191b90087b6cb8
unknown <Haim.Turkel@ORYMHAIMT.clearforest.com>
2016-02-16 11:09:37 +0200

    add logger

--- server/TTTCommander/src/main/java/com/tikal/ttt/configuration/Application.java
+++ server/TTTCommander/src/main/java/com/tikal/ttt/configuration/Application.java
@@ -1,15 +1,19 @@
-package com.tikal.ttt;
+package com.tikal.ttt.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 /**
  * Created by Haim.Turkel on 2/16/2016.
  */
 @SpringBootApplication
 public class Application {
+    static Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
+        logger.info("start");
         SpringApplication.run(Application.class, args);
     }
 }
