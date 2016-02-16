dda23266c2d6b4feb89f294194e4bc7e8a48a526
Yoram Michaeli <yorammi@tikalk.com>
2016-02-16 14:31:14 +0200

    changes in Slack settings

--- server/TTTCommander/src/main/java/com/tikal/ttt/slack/Slacklog.java
+++ server/TTTCommander/src/main/java/com/tikal/ttt/slack/Slacklog.java
@@ -14,7 +14,7 @@ import java.net.URL;
 public class Slacklog{
 
     public static void log(String message) throws URISyntaxException {
-        String payload = String.format("{\"channel\": \"#ttt-log\", \"username\": \"webhookbot\", \"text\": \"%s\", \"icon_emoji\": \":ghost:\"}",message);
+        String payload = String.format("{\"channel\": \"#ttt-log\", \"username\": \"tikal\", \"text\": \"%s\", \"icon_emoji\": \":ttt:\"}",message);
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.postForObject(new URI("https://hooks.slack.com/services/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"), payload,String.class);
     }
