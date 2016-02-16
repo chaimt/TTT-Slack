bce53b1e9fec1dd9f412949c749b1f1a91fd1a03
unknown <Haim.Turkel@ORYMHAIMT.clearforest.com>
2016-02-16 10:41:35 +0200

    basic config

---
+++ server/TTTCommander/src/main/java/com/tikal/ttt/Application.java
@@ -0,0 +1,7 @@
+package com.tikal.ttt;
+
+/**
+ * Created by Haim.Turkel on 2/16/2016.
+ */
+public class Application {
+}
---
+++ server/TTTCommander/src/main/java/com/tikal/ttt/configuration/Application.java
@@ -0,0 +1,15 @@
+package com.tikal.ttt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Created by Haim.Turkel on 2/16/2016.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
